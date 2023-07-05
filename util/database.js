2f1e2706f9a7c181d2b66523bce18298656fdd20
denyskarakycha <denys.karakycha@gmail.com>
2023-07-05 20:24:23 +0300

    Added sequelize module.

--- util/database.js
+++ util/database.js
@@ -1,11 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '295q6722822'
-});
+const { Sequelize } = require("sequelize")
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: '295q6722822'
+// });
 
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', '295q6722822', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
